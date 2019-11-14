376db6321c6bc800e50dc47d1e51105f681f63f6
Venkat3401 <57175506+Venkat3401@users.noreply.github.com>
2019-11-14 11:36:33 +0530

    Add project files.

---
+++ TSP_SeleniumAutomation/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TSP_SeleniumAutomation
+{
+    [TestClass]
+    public class LoginPage
+    {
+        [TestMethod]
+        public void Verifytologin()
+        {
+
+        }
+    }
+}
